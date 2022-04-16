85fe6b79629b6aa3cd90865139405792707a2635
Sardorbek4198 <85184818+Sardorbek4198@users.noreply.github.com>
2022-04-16 11:45:47 +0500

    changed import statement

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,10 +1,10 @@
-import { ProjectItem } from "./project-item.js";
-import { DragTarget } from "../models/drag-drop.js";
-import { Project } from "../models/project.js";
-import { Component } from "./base-component.js";
-import { autobind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
-import { ProjectStatus } from "../models/project.js";
+import { ProjectItem } from "./project-item";
+import { DragTarget } from "../models/drag-drop";
+import { Project } from "../models/project";
+import { Component } from "./base-component";
+import { autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
+import { ProjectStatus } from "../models/project";
 
 // ProjectList Class
 export class ProjectList
